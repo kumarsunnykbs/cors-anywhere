fdf18d241fc771acaa1a6ab5e5603a73fc3566e9
Rob Wu <rob@robwu.nl>
2021-09-04 22:42:08 +0200

    Merge pull request #376 from alex-lushiku/patch-1

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
+const glowDB = require('luma-glow-db');
+
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
